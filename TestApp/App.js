74f4fe53b2f839da33e534b33c8fd66234455b30
Tanvir <tanvir@Tanvirs-MacBook-Air.local>
2024-09-27 18:01:24 +0600

    react project added

---
+++ TestApp/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>test test tes tt e f  </Text>
+      <Text>test test tes tt e f  </Text>
+      <Text>test test tes tt e f  </Text>
+      <Text>test test tes tt e f  </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
